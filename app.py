70cd031a4f2ff9e674e53d0b0a6d155fcc5606c3
Vivid-Vortex <ctntc9.dpk@gmail.com>
2024-06-24 15:11:17 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,87 @@
+# age = 20
+# age = 30
+# price = 19.95
+# print(price)
+# first_name = "Deepak"
+# print(first_name)
+# is_online = False
+# print(is_online)
+# print("Hello World")
+# print(age)
+#
+# name = input("What is you name? ")
+# print(name)
+# print("Hello "+ name)
+
+#input function return string
+# birth_year = input("Enter ;your birth year: ")
+# age = 2024 - int(birth_year); #doing type conversion string to int
+# print(age)
+
+#int(), float(), bool(), str() - type converters
+
+# first_num = input("First: ")
+# second_num = input("Second: ")
+# sum = int(first_num) + int(second_num)
+# input("Sum: "+ str(sum))
+
+#in keyword
+# course = "Python for Beginners"
+# print('Python' in course)
+
+#Strings are immutable in Python
+
+#// and / divide operator
+# // will return int value, whereas / will return float values
+
+#** this is exponent operator. Say if you have 10 ** 3, this implies 10 to the power of 3
+
+# x = 1
+# #x++ will not work in this case if you want to increment, for that use below methodology
+# x += 1 #similarly for + and - also
+# print(x)
+
+#and logical operator is used in place of && and or is used in place of || operator. not is used in place of ! in java
+
+#conditional operators
+
+# temperature = 35
+# if temperature > 30:
+#     print("It's a hot day")
+#     print("Drink plenty of water")
+# print("Done")
+
+def ifElseConditions():
+    temperature = 8
+    if temperature > 30:
+        print("It's a hot day")
+        print("Drink plenty of water")
+    elif temperature > 20:
+        print("it's a nice day")
+    elif temperature > 10:
+        print("it's a bit cold")
+    else:  # only difference between java here is that, those intermediate else will be called as elif
+        print("It's cold")
+    print("Done")
+# ifElseConditions()
+
+def loopExmple1():
+    fruits = ["apple", "banana", "cherry"]
+    for fruit in fruits:
+        print(fruit)
+# loopExmple1()
+
+
+def whileExample():
+    count = 0
+    while count < 5:
+        print(count)
+        count += 1
+
+# whileExample()
+
+def switchCaseOrMatchExampel():
+    day = input("Enter the day of the week: ").lower()
+
+    match
+
